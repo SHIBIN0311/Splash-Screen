3adbfdc27258ddb6db57bb030f6152eaeef0d8e3
SHIBIN0311 <toxicaza@gmai8l.com>
2024-02-05 14:21:02 +0530

    modified

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.teal,
       ),
       body: const Center(
-        child: Text("Welcome budy"),
+        child: Text("Welcome buddy"),
       ),
     );
   }
